2187eb0db5af25319009468119b8b6bed3ba8ffb
Gonçalo <Boomiespsn@gmail.com>
2016-09-29 20:12:45 +0100

    AI

---
+++ SLfase0.sql
@@ -0,0 +1,8 @@
+create table student(
+	nome varchar(50) NOT NULL, numero int,
+	primary key(numero)
+)
+
+insert into student(nome, numero) values
+('Gonçalo Veloso', 41482),
+('André Carvalho', 41839)
