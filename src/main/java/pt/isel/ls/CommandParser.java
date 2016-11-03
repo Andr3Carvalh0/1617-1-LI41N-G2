a53c724adb697399e5569a86d975727cf043fb02
GoncaloV <41482@alunos.isel.ip.pt>
2016-11-03 22:35:32 +0000

    Fixed commandparser.

--- src/main/java/pt/isel/ls/CommandParser.java
+++ src/main/java/pt/isel/ls/CommandParser.java
@@ -10,24 +10,40 @@ public class CommandParser {
     private String[] headersArray;
 
     public CommandParser(String[] args) throws Exception {
-            method = args[0];
-            path = args[1].split("/");
-            params = new HashMap<String, String>();
-            if (args.length > 2) {
-                String[] paramsArray = args[args.length - 1].split("&");
-                String[] aux;
-                for (int i = 0; i < paramsArray.length; i++) {
-                    aux = paramsArray[i].split("=");
-                    aux[1] = aux[1].replace('+', ' ');
-                    params.put(aux[0], aux[1]);
-                }
-                if (args.length == 4 && method.equals("GET"))
-                    headersArray = args[2].split("|");
-                for (int i = 0; i < headersArray.length; i++) {
-                    headers.put(headersArray[i].split(":")[0],headersArray[i].split(":")[1]);
-                }
-            }
+        method = args[0];
+        path = args[1].split("/");
+        if (args.length > 2) {
+            if(args[2].contains("=") && args[2].contains(":")) throw new Exception("Não sao permitidos parâmetros com ':' ou headers com '='");
 
+            if(args[2].contains("="))
+                setParams(args,2);
+            else if(args[2].contains(":"))
+                setHeaders(args);
+
+            if (args.length == 4)
+                if(args[3].contains("+"))
+                    setParams(args,3);
+        }
+
+    }
+
+    private void setHeaders(String[] args) {
+        headersArray = args[2].split("|");
+        headers = new HashMap<String, String>();
+        for (int i = 0; i < headersArray.length; i++) {
+            headers.put(headersArray[i].split(":")[0], headersArray[i].split(":")[1]);
+        }
+    }
+
+    private void setParams(String[] args, int n) {
+        params = new HashMap<String, String>();
+        String[] paramsArray = args[n].split("&");
+        String[] aux;
+        for (int i = 0; i < paramsArray.length; i++) {
+            aux = paramsArray[i].split("=");
+            aux[1] = aux[1].replace('+', ' ');
+            params.put(aux[0], aux[1]);
+        }
     }
 
     public String getMethod() {
