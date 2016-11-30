52630fe9ed41a7b89f5c56e62ad9e411f4107ba3
André Carvalho <andre.daniel.carvalho@gmail.com>
2016-11-30 23:40:08 +0000

    UI improvements to the HTML

--- src/main/java/pt/isel/ls/Utils/Output/EmptyObject.java
+++ src/main/java/pt/isel/ls/Utils/Output/EmptyObject.java
@@ -1,11 +1,20 @@
 package pt.isel.ls.Utils.Output;
 
+import java.util.Random;
+
 //This class is just to store the user command.
 //This will be used when the user execute some query and the result is empty
 class EmptyObject {
     private String query;
+    private String message;
+
+    private final String[] messages = {"Blame the stupid SQL", "Blame the idiots on Group 2", "Try again", "Just go take a break, and try later"};
 
     EmptyObject(String query){
         this.query = query;
+        Random r = new Random();
+
+        message = messages[r.nextInt(messages.length)];
+
     }
 }
