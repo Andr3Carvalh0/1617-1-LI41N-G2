4ff9b40eeb2fd87ff9cc1e20b04fb6d3a6bac002
GoncaloV <41482@alunos.isel.ip.pt>
2017-01-02 20:18:28 +0000

    Posts done?

--- src/main/java/pt/isel/ls/Server/Service.java
+++ src/main/java/pt/isel/ls/Server/Service.java
@@ -1,20 +1,21 @@
 package pt.isel.ls.Server;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.charset.Charset;
-import java.sql.Connection;
-import java.util.HashMap;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import pt.isel.ls.CommandParser;
 import pt.isel.ls.Commands.Command;
 import pt.isel.ls.Router;
 import pt.isel.ls.Server.Utils.GetRootInfo;
 import pt.isel.ls.Utils.GetConnection;
 import pt.isel.ls.Utils.Output.CustomPrinter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Service extends HttpServlet {
     private static CustomPrinter cPrinter = new CustomPrinter();
@@ -34,7 +35,7 @@ public class Service extends HttpServlet {
 
             //This is useless but you never know...
             map.put("file-name", req.getHeader("file-name"));
-            if(req.getPathInfo().equals("/")){
+            if (req.getPathInfo().equals("/")) {
                 Connection con = GetConnection.connect(false);
                 try {
                     respBody = cPrinter.print(new GetRootInfo().execute(null, con), map, req.getRequestURI());
@@ -45,8 +46,7 @@ public class Service extends HttpServlet {
                         con.close();
                     }
                 }
-            }
-             else if (req.getPathInfo().equals("/about")) {
+            } else if (req.getPathInfo().equals("/about")) {
                 respBody = cPrinter.print(req.getPathInfo(), map, req.getRequestURI());
                 respBodyBytes = respBody.getBytes(utf8);
                 resp.setStatus(200);
@@ -55,7 +55,7 @@ public class Service extends HttpServlet {
                 CommandParser cparser = new CommandParser(new String[]{req.getMethod(), req.getPathInfo(), getContentType(req.getHeader("accept"))});
                 Router r = new Router(cparser.getMethod(), cparser.getPath(), cparser.getParams());
                 Command c = r.Route();
-                try{
+                try {
                     Object obj = r.run(c);
 
                     if (c == null) {
@@ -64,14 +64,13 @@ public class Service extends HttpServlet {
                         respBodyBytes = respBody.getBytes(utf8);
                         resp.setStatus(404);
 
-                    }else {
+                    } else {
 
                         respBody = cPrinter.print(obj, map, req.getRequestURI());
                         respBodyBytes = respBody.getBytes(utf8);
                         resp.setStatus(200);
                     }
-                }
-                catch (Exception e){
+                } catch (Exception e) {
                     respBody = cPrinter.print(null, map, req.getRequestURI());
 
                     respBodyBytes = respBody.getBytes(utf8);
@@ -102,5 +101,76 @@ public class Service extends HttpServlet {
             return "text/plain";
 
         return "text/html";
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try {
+            Map<String, String[]> col = req.getParameterMap();
+            Charset utf8 = Charset.forName("utf-8");
+
+            resp.setContentType(String.format(getContentType(req.getHeader("accept")) + "; charset=%s", utf8.name()));
+
+            String respBody;
+            byte[] respBodyBytes;
+            HashMap<String, String> map = new HashMap<>();
+
+            map.put("accept", getContentType(req.getHeader("accept")));
+
+            //This is useless but you never know...
+            map.put("file-name", req.getHeader("file-name"));
+
+            String params = formatParams(col);
+            CommandParser cparser = new CommandParser(new String[]{req.getMethod(), req.getPathInfo(), params});
+            Router r = new Router(cparser.getMethod(), cparser.getPath(), cparser.getParams());
+            Command c = r.Route();
+            try {
+                Object obj = r.run(c);
+                if (c == null) {
+                    respBody = cPrinter.print("not_found", map, req.getRequestURI());
+
+                    respBodyBytes = respBody.getBytes(utf8);
+                    resp.setStatus(404);
+
+                } else {
+                    System.out.println(obj);
+                    if(!req.getPathInfo().matches(".*\\d+.*")) {
+                        resp.sendRedirect(req.getPathInfo() + "/" + obj);
+                    }
+                    else {
+                        resp.sendRedirect(rebuildURL(req.getPathInfo()));
+                    }
+                    return;
+                }
+            } catch (Exception e) {
+                respBody = cPrinter.print(null, map, req.getRequestURI());
+
+                respBodyBytes = respBody.getBytes(utf8);
+                resp.setStatus(500);
+
+            }
+
+            resp.setContentLength(respBodyBytes.length);
+            OutputStream os = resp.getOutputStream();
+            os.write(respBodyBytes);
+            os.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String rebuildURL(String pathInfo) {
+        String[] p = pathInfo.split("/");
+        return "/"+p[1]+"/"+p[2]+"/";
+    }
+
+    private String formatParams(Map<String, String[]> col) {
+        String toReturn = "";
+        for (String key : col.keySet()) {
+            if(!col.get(key)[0].equals(""))
+            toReturn += key + "=" + col.get(key)[0].replace(' ', '+') + "&";
+        }
+        return toReturn.substring(0, toReturn.length() - 1);
     }
 }
